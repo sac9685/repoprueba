28afdfae87bf18c5601bdf759bd1c8fb63cf6e43
Sergio Conti <contisa_2000@yahoo.com.ar>
2025-02-22 19:19:32 -0300

    Little script to create a few tables in the database

---
+++ myscript.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(40) NOT NULL UNIQUE
+);
+CREATE TABLE post (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    title VARCHAR(100) NOT NULL UNIQUE
+);
+CREATE TABLE comments(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    post_id INTEGER NOT NULL REFERENCES post(id),
+    body VARCHAR(500) NOT NULL
+);
